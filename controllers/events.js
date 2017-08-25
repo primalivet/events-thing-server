0b1b0994f3526300d100b7dde22d8e5bee116497
Gustaf Holm <gustaf.holm@goteborg.com>
2017-08-25 15:39:44 +0200

    comments

--- controllers/events.js
+++ controllers/events.js
@@ -1,9 +1,15 @@
+// import dependencies
 const moment = require('moment-timezone');
 const pagination = require('../helpers/pagination.js');
 const Event = require('../models/event');
 
 /**
- * validationSchema
+ * schema
+ * ---
+ * the object below contains a validation schema for
+ * the different controllers in this file. It's used
+ * for request validation inside the ../routes/events.js
+ * file
  */
 
 exports.schema = {
@@ -59,10 +65,19 @@ exports.schema = {
 
 /**
  * getEvents
+ * ---
+ * the getEvents controller retrives events from the db
+ * based on the request query. It returns a promise that
+ * resolves to an object with the events and some other
+ * information on success. This controller is used inside
+ * ../routes/events.js
  */
 // eslint-disable-next-line no-unused-vars
 exports.getEvents = (req, res) =>
+  // return a new promise and add support for async/await
+  // inside the promise
   new Promise(async (resolve, reject) => {
+    // declare variables for later use
     let events;
     let count;
 
@@ -71,10 +86,16 @@ exports.getEvents = (req, res) =>
       ? moment.tz(req.query.date, 'Europe/Stockholm').toISOString()
       : moment.tz(0, 'HH', 'Europe/Stockholm').toISOString();
 
+    // check if offset and limit query exists and assign them
+    // these two variables are used for pagination support
     const offset = req.query.offset ? Number(req.query.offset) : 0;
     const limit = req.query.limit ? Number(req.query.limit) : 3;
 
     try {
+      // try to retrive events from the db based on the query
+      // only retrive events with a starting date greater than the
+      // "start of today" (the current date with 00:00:00 hours).
+      // populate the author field.
       events = await Event
         .find()
         .where('start')
@@ -84,29 +105,41 @@ exports.getEvents = (req, res) =>
         .skip(offset)
         .populate('author');
     } catch (error) {
+      // reject the promise if there was an error when trying
+      // to retive the events
       reject({
         message: 'Could not retrive events',
         error,
       });
     }
 
+    // if there were no events in the db, reject the promise
     if (!events.length) reject({ message: 'No events 😞' });
 
     try {
+      // try getting the total count of events without pagination
+      // limit and offset but with greater date than today
       count = await Event
         .where('start')
         .gt(date)
         .sort('start')
         .count();
     } catch (error) {
+      // reject if there was an error in the count db request
       reject({
         message: 'Could not retrive events total count',
         error,
       });
     }
 
+    // here I use a pagination "helper", it takes an object with
+    // stuff based on the query parameters above. Then I destucture
+    // stuff I need into seperate variables which are used when the
+    // promise resolves below.
     const { self, first, last, next, prev } = pagination({ req, count, limit, offset, date });
 
+    // resolve the promise to an object with all the stuff
+    // we've retrived above.
     resolve({
       items: events,
       meta: { count, offset, limit, date },
